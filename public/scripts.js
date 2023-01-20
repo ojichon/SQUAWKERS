55988a859c676eac22b41e51aa02ac614e61b092
ojichon <misohero@gmail.com>
2023-01-19 21:38:17 -0500

    stuff

--- public/scripts.js
+++
@@ -1,7 +0,0 @@
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(form);
-  const tweet = formData.get('tweet-text');
-  console.log(tweet);
-});
--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 const path = require('path')
 
 app.use(express.json());
+app.use(express.static('/public'))
 app.use(express.static(__dirname + '/public' ));
+app.use(express.static(path.join(__dirname, "/public")));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
